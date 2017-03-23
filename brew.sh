f286ba8dc30500a582cabd7135a84daae01c02a8
Evan Klemen <evan.klemen@wbdcorp.com>
2017-03-23 17:45:27 -0400

    breaking scripts out into .sh files

---
+++ brew.sh
@@ -0,0 +1,20 @@
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      brew update
+      brew install git tree fasd yarn mysql nginx node ruby
+  fi
+
+  echo 'Tweaking OS X...'
+    source 'osx.sh'
+
+  # http://github.com/sindresorhus/quick-look-plugins
+  echo 'Installing brew cask plugins...'
+    brew tap phinze/homebrew-cask
+    brew tap homebrew/php
+    brew install caskroom/cask/brew-cask
+    brew cask install google-chrome slack qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv
+    brew install php71 --with-apache
+fi
---
+++ osx.sh
@@ -0,0 +1,53 @@
+################################
+#### OSX default overwrites ####
+################################
+# ~~~ these are .sh ready ~~~~ #
+
+# Increase OSX default key repeate rate
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms) can only be 1 or 2
+
+# Disable animations when opening and closing windows.
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# Show battery life percentage.
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=0
+
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+# ===========================================================
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write com.apple.AppleMultitouchTrackpad Clicking 1
+
+# SSD tweaks
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+# Finder
+# ======
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
